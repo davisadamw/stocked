193a42fa61236e609863bb60024f2149b1ec20d6
Adam Davis <davisadamw@gmail.com>
2019-11-01 15:47:57 -0700

    renamed single assignment step function script and updated documentation

--- R/single_assignment_step.R
+++ R/single_assignment_step.R
@@ -1,21 +1,27 @@
 
-# calculate A, run assignment
-#' Title
+#' Run a single assignment step for the model
 #'
-#' @param data
-#' @param evs_this_iter
-#' @param iteration
-#' @param p
-#' @param q
+#' Run a single round of the model, assigning units equal to total_to_assign / n_steps distributed among the regions included in the model
+#' This will usually be called by [run_assignment()], either directly or via [run_assignment_with_variable_bp]
 #'
-#' @return
+#' @param data Data frame output from previous step
+#' @param n_this_iter Number to assign this step, probably total / n_steps
+#' @param iteration Iteration step, integer > 0
+#' @param p Innovation parameter, numeric 0-1
+#' @param q Immitation parameter, numeric 0-1
+#'
+#' @return Data frame matching format of \code{data}.
+#'
 #' @export
 #' @importFrom magrittr "%>%"
+#' @importFrom dplyr select
+#' @importFrom dplyr one_of
 #' @importFrom dplyr rename
 #' @importFrom dplyr mutate
+#' @importFrom rlang .data
 #'
 #' @examples
-assign_PEVs_step <- function(data, evs_this_iter, iteration, p, q) {
+single_assignment_step <- function(data, n_this_iter, iteration, p, q) {
 
   # first, drop columns that get updated each round, ignoring warning about missings
   suppressWarnings({
@@ -26,16 +32,16 @@ assign_PEVs_step <- function(data, evs_this_iter, iteration, p, q) {
   # rename _current vars to _prev
   data_trimmed %>%
     # rename the values of market and M from previous round
-    rename(market_prev = market_curr,
-           M_prev      = M_curr) %>%
+    rename(market_prev = .data$market_curr,
+           M_prev      = .data$M_curr) %>%
     mutate(iter_no = iteration,
-           A       = bass(M_prev, p, q),
-           W       = base_rate * market_limit * A,
-           W_share = W / sum(W),
+           A       = bass(.data$M_prev, p, q),
+           W       = .data$base_rate * .data$market_limit * .data$A,
+           W_share = .data$W / sum(.data$W),
            # assignment is weight as share of total weight or remaining capacity, whichever is smaller
-           assign      = pmin(W_share * evs_this_iter,
-                              market_limit - market_prev),
-           market_curr = market_prev + assign,
-           M_curr      = market_curr / market_limit)
+           assign      = pmin(.data$W_share * n_this_iter,
+                              .data$market_limit - .data$market_prev),
+           market_curr = .data$market_prev + .data$assign,
+           M_curr      = .data$market_curr / .data$market_limit)
 
 }

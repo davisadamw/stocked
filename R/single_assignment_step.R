82a62ce77cf9d4fb2b68fca91e97abe37e8590dc
Adam Davis <davisadamw@gmail.com>
2019-11-25 16:02:58 -0800

    cleaned single_assingment_step to minimize column creation / destruction ... shaves 3/4 of total time for CA-size dataset

--- R/single_assignment_step.R
+++ R/single_assignment_step.R
@@ -12,34 +12,30 @@
 #'
 #' @return Data frame matching format of \code{data}.
 #'
-#' @importFrom magrittr "%>%"
-#' @importFrom dplyr select
-#' @importFrom dplyr one_of
-#' @importFrom dplyr rename
 #' @importFrom dplyr mutate
 #' @importFrom rlang .data
 #'
 single_assignment_step <- function(data, n_this_iter, iteration, p, q) {
 
   # first, drop columns that get updated each round, ignoring warning about missings
-  suppressWarnings({
-    data_trimmed <- data %>%
-      select(-one_of('market_prev', 'M_prev', 'W', 'W_share', 'assign'))
-  })
+  # suppressWarnings({
+  #   data_trimmed <- data %>%
+  #     select(-one_of('market_prev', 'M_prev', 'W', 'W_share', 'assign'))
+  # })
 
   # rename _current vars to _prev
-  data_trimmed %>%
-    # rename the values of market and M from previous round
-    rename(market_prev = .data$market_curr,
-           M_prev      = .data$M_curr) %>%
-    mutate(iter_no = iteration,
-           A       = bass(.data$M_prev, p, q),
-           W       = .data$base_rate * .data$market_limit * .data$A,
-           W_share = .data$W / sum(.data$W),
-           # assignment is weight as share of total weight or remaining capacity, whichever is smaller
-           assign      = pmin(.data$W_share * n_this_iter,
-                              .data$market_limit - .data$market_prev),
-           market_curr = .data$market_prev + .data$assign,
-           M_curr      = .data$market_curr / .data$market_limit)
+  # rename the values of market and M from previous round
+  mutate(data,
+         market_prev = .data$market_curr,
+         M_prev      = .data$M_curr,
+         iter_no = iteration,
+         A       = bass(.data$M_prev, p, q),
+         W       = .data$base_rate * .data$market_limit * .data$A,
+         W_share = .data$W / sum(.data$W),
+         # assignment is weight as share of total weight or remaining capacity, whichever is smaller
+         assign      = pmin(.data$W_share * n_this_iter,
+                            .data$market_limit - .data$market_prev),
+         market_curr = .data$market_prev + .data$assign,
+         M_curr      = .data$market_curr / .data$market_limit)
 
 }
